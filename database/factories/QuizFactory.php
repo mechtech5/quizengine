5d90854e497f3113ee3fd02d67ff7c901ef0615d
Ayush Likhar <neontechhead@gmail.com>
2019-07-02 23:32:48 +0530

    added necessary packages

---
+++ database/factories/QuizFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Quiz;
+use Faker\Generator as Faker;
+
+$factory->define(Quiz::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
